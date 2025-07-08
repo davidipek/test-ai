67e33cc4c78b38330aaec5704b3ead3b878fb8ab
David Ipek <dipek5555@gmail.com>
2025-07-08 15:40:28 +0200

    Fix: Byt experimental_rerun mot egen rerun-funktion och förbättra vyer

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-PAGE_TITLE = "AutoProfit"
-WEEKS_AHEAD = 8  # antal veckor att prognostisera framåt
+# config.py
+
+DATA_FILE = "data/budgetkoll.db"
+PAGE_TITLE = "BudgetKoll SaaS"
+WEEKS_AHEAD = 12  # prognoslängd i veckor
